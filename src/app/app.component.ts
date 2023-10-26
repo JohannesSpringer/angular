3b41bbc71c20b0baa7c39838c1281376b45714fa
Johannes Springer <johannes.springer.01@gmx.de>
2023-10-26 06:34:35 +0200

    daily commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements OnInit {
-  images = ['dove-2516641_1920.jpg', 'mountains-615428_1920.jpg', 'road-1072821_1920.jpg'];
-  headlines = [
-    'Bring engineering to the next level',
-    'Next level shit',
-    'Improvement is the way to win'
-  ];
+export class AppComponent {
   currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000);
-  }
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.sass']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['dove-2516641_1920.jpg', 'mountains-615428_1920.jpg', 'road-1072821_1920.jpg'];
+  headlines = [
+    'Bring engineering to the next level',
+    'Next level shit',
+    'Improvement is the way to win'
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+}
